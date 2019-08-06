624f9637ff28c59707478c660c6bc9225c3c53bc
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-08-07 00:17:10 +0200

    inline

--- share.js
+++ share.js
@@ -1,20 +1,27 @@
 
 
-function loop () {
+function show_dir (path) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
-            document.getElementById('img_show').innerHTML = this.responseText;
+            var select_div = document.getElementById('select');
+            for (d in data.list) {
+                var item_div = document.createElement("div");
+                item_div.innerHTML = data.list[d].path;
+                item_div.className = "item";
+                select_div.appendChild(item_div);
+                select_div.appendChild(document.createTextNode(" "));
+            }
+            document.getElementById('controls').innerHTML = path;
         }
     };
     request.open("POST", "share_list_files", true);
     var req_data = {};
-    req_data.dir = "/";
+    req_data.dir = path;
     req_data.thumbnail = true;
     request.send(JSON.stringify(req_data));
 }
 
 
-setTimeout(loop, 1000);
-
+show_dir ("/links");
