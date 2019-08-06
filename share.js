7adad776d8e6246a7b035282d9fc8b913e9eebab
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-08-06 10:16:16 +0200

    something basic

--- share.js
+++ share.js
@@ -1,7 +1,15 @@
 
 
 function loop () {
-    document.getElementById('img_show').innerHTML = "ch";
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            document.getElementById('img_show').innerHTML = data.name;
+        }
+    };
+    request.open("GET", "demo.json", true);
+    request.send();
 }
 
 
